73801f3044df6b46e06a61913c670db8f0339f6a
mrprince88 <ps234110>
2020-04-19 19:19:14 +0530

    initial commit

---
+++ File_IO_in_Java/Main.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+	static private final String INPUT = "input.txt";
+	static private final String OUTPUT = "output.txt";
+
+	public static void main(String[] args) {
+
+		long startTime = System.nanoTime() / 1000000000;
+		FileInputStream instream = null;
+		PrintStream outstream = null;
+
+		try {
+			instream = new FileInputStream(INPUT);
+			outstream = new PrintStream(new FileOutputStream(OUTPUT));
+			System.setIn(instream);
+			System.setOut(outstream);
+		} catch (Exception e) {
+			System.err.println("Error");
+		}
+
+		Scanner s = new Scanner(System.in);
+
+
+		double endTime   = System.nanoTime() / 1000000000;
+		double totalTime = endTime - startTime;
+		System.err.format("%.8f\n", totalTime);
+		return;
+	}
+}
